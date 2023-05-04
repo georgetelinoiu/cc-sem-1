138b0cc4793ff141660e7d27b80bb626b6138061
georgetelinoiu <telinoiugeorgian@gmail.com>
2023-05-04 18:54:55 +0300

    initial display of data

--- js/utils/functions.js
+++ js/utils/functions.js
@@ -1,5 +1,5 @@
 // js/utils/functions.js
-import {connectToDatabase,} from '@/lib/mongodb.js';
+import {connectToDhatabase,} from '@/lib/mongodb.js';
 
 export const getCollection = async collectionName => {
 	const {database,} = await connectToDatabase();
